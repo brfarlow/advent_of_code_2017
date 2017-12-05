aa16f3e6e8aeb68daf0b5c81409fc575244e0cde
Branden Farlow <brfarlow@gmail.com>
2017-12-04 23:01:37 -0500

    Simplfies day4 part2

--- day4/day4-part2.py
+++ day4/day4-part2.py
@@ -2,7 +2,7 @@ with open('input.txt') as f:
     valid_passphrases = 0
 
     for line in f.readlines():
-        phrases = list(map(lambda x: ('').join(sorted(list(x))), line.split()))
+        phrases = [('').join(sorted(x)) for x in line.split()]
         if len(phrases) == len(set(phrases)):
             valid_passphrases += 1
 
